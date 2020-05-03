9c0e214d175d8fa7d0a38142538948600e7f8b82
sarohask <saroha.sk@gmail.com>
2020-05-04 02:31:07 +0530

    changed to TrGEMG4

--- condor_calo_sim_wrapper.sh
+++ condor_calo_sim_wrapper.sh
@@ -2,8 +2,10 @@
 
 hostname
 source /usr/local/root/bin/thisroot.sh
-export PATH=$PATH:/usr/local/root/include
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/root/lib
+export ROOTSYS=/usr/local/root
+export PATH=$PATH:$ROOTSYS/bin
+export PATH=$PATH:$ROOTSYS/include
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOTSYS/lib
 
 mkdir EMCalo_build
 cd EMCalo_build
